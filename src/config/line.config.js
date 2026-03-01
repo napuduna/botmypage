ec8dfed38cbb3290366649ea2278352e84b26f1b
Thirakan <thirakan.weef64@gmail.com>
2026-03-01 14:59:24 +0700

    fix(line-config): correct API endpoint from api.line.biz to api.line.me

--- src/config/line.config.js
+++ src/config/line.config.js
@@ -3,8 +3,8 @@ const logger = require('../utils/logger');
 const lineConfig = {
   CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET,
-  API_ENDPOINT: 'https://api.line.biz/v1',
-  PUSH_MESSAGE_ENDPOINT: 'https://api.line.biz/v1/bot/message/push',
+  API_ENDPOINT: 'https://api.line.me/v2',
+  PUSH_MESSAGE_ENDPOINT: 'https://api.line.me/v2/bot/message/push',
 };
 
 const validateLineConfig = () => {
